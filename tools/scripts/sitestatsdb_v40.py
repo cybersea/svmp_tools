d9863d3a2197532315688847afe008061cbde8c7
Allison Bailey <allison@soundgis.com>
2015-05-18 11:39:44 -0700

    Add Transects Class and transect-specific properties to Site class

---
+++ tools/scripts/sitestatsdb_v40.py
@@ -0,0 +1,42 @@
+__author__ = 'allison'
+
+import svmpUtils as utils
+import arcpy
+import svmp_10x as svmp
+# Import the custom Exception class for handling errors
+from svmp_exceptions import SvmpToolsError
+import svmpUtils as utils
+
+if __name__ == '__main__':
+
+    mysite = svmp.Site("swh1626", "2014", "Native_SG",
+                  r"Z:\Users\allison\projects\dnr_svmp2015\eelgrass_svmp\00-14_monitoring_results\transect_data\svmp_TD_abtest.mdb",
+                  r"Z:\Users\allison\projects\dnr_svmp2015\eelgrass_svmp\00-14_monitoring_results\svmp_site_info.mdb\sample_polygons",
+                  r"Z:\Users\allison\projects\dnr_svmp2015\eelgrass_svmp\2014_test\site_folders")
+
+    print "SITE: %s" % (mysite.id)
+    # print "Transect Feature Class: " + mysite.transect_pt_fc
+    # print "Transect Feature Class exists? %s " % (mysite.transect_pt_fc_exists)
+    # print "Control File: " + mysite.ctl_file
+    # print "Control File exists? %s" % (mysite.ctl_file_exists)
+    # print "Veg Column:" + mysite.veg_col
+    # print "Veg Column exists? %s" % (mysite.veg_col_exists)
+    # print "Veg Exists? %s" % (mysite.veg_exists)
+    # print "Sample Polygon ID: %s_%s" % (mysite.id, mysite.sampling_occasion)
+    # print "Sample Polygon Exists? %s" % (mysite.sample_poly_exists)
+    print "Transects: %s" % (mysite.transect_list)
+    print mysite.transect_ln_fc
+    print mysite.transect_ln_clip_fc
+
+    mytransects = svmp.Transects(mysite)
+
+    print mytransects.transect_results_ids
+    print mytransects.trktypes
+    print mytransects.maxflags
+    print mytransects.minflags
+    print mytransects.transect_list
+    print mytransects.mintransdeps
+    print mytransects.maxtransdeps
+    print mytransects.minvegdeps
+    print mytransects.maxvegdeps
+
--- tools/scripts/svmp_10x.py
+++ tools/scripts/svmp_10x.py
@@ -15,19 +15,21 @@ import numpy
 class Site:
     """ Represents the SVMP data for an individual site
 
-    Attributes
+    Properties
     id -- individual site identifier
     sampling_occasion -- sampling occasion (for example, year)
+    sitestat_id -- unique identifier for site and sampling occasion combination
     veg_col -- vegetation column of interest
     veg_col_exists -- Flag for existence of vegetation column in transect_pt_fc
     transect_gdb -- geodatabase containing transect point feature classes
     transect_pt_fc -- Transect point feature class (full path)
     transect_pt_fc_exists -- Flag for existence of transect point feature class
     sample_poly_fc -- Feature Class containing all sample polygons
-    sample_poly -- Sample polygon feature
     sample_poly_exists -- Flag for existence of sample polygon
+    sample_poly -- Sample polygon feature
     ctl_file -- Control file name (full path)
     ctl_file_exists -- Flag for existence of control file
+    transects -- list of transects at the site
 
     """
 
@@ -35,6 +37,8 @@ class Site:
         self.id = id
         self.sampling_occasion = sampling_occasion
         self.veg_col = veg_col
+        self.sitestat_id = "".join((self.id,"_",self.sampling_occasion))
+        self.site_results_id = "".join((self.id,"_",self.sampling_occasion,"_",veg_col))
         self.transect_gdb = transect_gdb
         self.sample_poly_fc = sample_poly_fc
         self.ctl_directory = ctl_directory
@@ -84,7 +88,6 @@ class Site:
         if self.veg_col_exists:
             # Make NumPy Array from table and see if it returns a result
             results = arcpy.da.TableToNumPyArray(self.transect_pt_fc, self.veg_col)
-            print "Max of %s: %s" % (self.veg_col, results[self.veg_col].max())
             if results[self.veg_col].max() > 0:
                  return True
             else:
@@ -96,9 +99,8 @@ class Site:
     def sample_poly_exists(self):
         """ Flag for existence of sample polygon"""
         # Build where clause like this: sitestat_id = "hdc2334_2014"
-        samppoly_id = "".join((self.id,"_",self.sampling_occasion))
         delimited_field = arcpy.AddFieldDelimiters(self.sample_poly_fc, utils.sitestatidCol)
-        where_clause = delimited_field + " = " + "'%s'" % (samppoly_id)
+        where_clause = delimited_field + " = " + "'%s'" % (self.sitestat_id)
         # Make NumPy Array from table and see if it returns a result
         results = arcpy.da.TableToNumPyArray(self.sample_poly_fc, utils.sitestatidCol, where_clause)
         #print results - for debugging
@@ -106,6 +108,191 @@ class Site:
             return True
         else:
             return False
+
+    @property
+    def transect_list(self):
+        """ Returns a list of unique transects at the site """
+        if self.transect_pt_fc_exists:
+            results = arcpy.da.TableToNumPyArray(self.transect_pt_fc, utils.trkCol)
+            # print results
+            # print results.dtype.names
+            # print results.dtype
+            # print numpy.unique(results)
+            # print numpy.unique(results).tolist()
+            # print numpy.unique(results).ndim
+            # print numpy.unique(results).shape
+            # print results.ndim
+            # print results.shape
+            return numpy.unique(results)[utils.trkCol].tolist()
+
+    @property
+    def transect_ln_fc(self):
+        """ Returns the the full path of a temporary feature class used to depict transect line segments """
+        fc_name = "%s_%s_transect_line" % (self.id, self.sampling_occasion)
+        return os.path.join(self.transect_gdb, fc_name)
+
+
+    @property
+    def transect_ln_clip_fc(self):
+        """ Returns the the full path of a temporary feature class used to depict CLIPPED transect line segments """
+        fc_name = "%s_%s_transect_line_clip" % (self.id, self.sampling_occasion)
+        return os.path.join(self.transect_gdb, fc_name)
+
+
+
+class Transects:
+    """ Represents the SVMP data for a set of transects at a Site
+
+    Properties
+    site -- Site object
+    transect_list -- list of transect ids at the site
+    transect_results_ids -- dictionary of transect id (key) and transect_results_id (value)
+    trktypes -- dictionary of transect id (key) and track type code (value)
+    maxflags -- dictionary of transect id (key) and flag indicating transect can be used for vegetation max depth
+    minflags -- dictionary of transect id (key) and flag indicating transect can be used for vegetation min depth
+    maxtransdeps -- dictionary of transect id (key) and maximum depth of transect (value)
+    mintransdeps -- dictionary of transect id (key) and minimum depth of transect  (value)
+    maxvegdeps -- dictionary of transect id (key) and maximum depth of vegetation on transect (value)
+    minvegdeps -- dictionary of transect id (key) and minimum depth of vegetation on transect (value)
+    """
+
+    def __init__(self,site):
+        self.site = site
+        self.transect_list = self.site.transect_list
+        self.get_trans_results_ids()
+        self.get_trkflags()
+        self.get_transdeps()
+        self.get_vegdeps()
+
+    def get_trans_results_ids(self):
+        self.transect_results_ids = {}
+        for t in self.site.transect_list:
+            self.transect_results_ids[t] = self.site.site_results_id + "_" + str(t).zfill(2)
+
+    def get_trkflags(self):
+        self.trktypes = {}
+        self.maxflags = {}
+        self.minflags = {}
+        """ This method is highly dependent upon the assumed structure of the control file
+        which has specific attributes and values listed line-by-line in sequential order.
+        For example:
+            Track, 1
+            TrackType,  SLPR
+            Perimeter,  Yes
+            BasalArea,  Yes
+            PatchIndex, Yes
+            MinDepth,   Yes
+            MaxDepth,   Yes
+        """
+        if self.site.ctl_file_exists:
+            for line in open(self.site.ctl_file):
+                line = line.strip()
+                [att,val] = line.split(",")
+                val = val.strip()
+                # Get Track Number
+                if att == utils.ctlTrkAtt:
+                    trk = int(val)
+                # Get Track Type
+                if att == utils.ctlTrktypeAtt:
+                    trktype = val
+                    self.trktypes[trk] = trktype
+                # Get Maximum Depth Flag
+                if att == utils.ctlMaxAtt:
+                    if val == utils.ctlYes:
+                        maxflag = 1
+                    elif val == utils.ctlNo:
+                        maxflag = 0
+                    self.maxflags[trk]= maxflag
+                # Get Minimum Depth Flag
+                if att == utils.ctlMinAtt:
+                    if val == utils.ctlYes:
+                        minflag = 1
+                    elif val == utils.ctlNo:
+                        minflag = 0
+                    self.minflags[trk]= minflag
+
+    def get_transdeps(self):
+        self.maxtransdeps = {}
+        self.mintransdeps = {}
+        if self.site.transect_pt_fc_exists:
+            delimited_field_dep = arcpy.AddFieldDelimiters(self.site.transect_pt_fc, utils.shpDepCol)
+            # Loop through all transect at the site
+            for t in self.site.transect_list:
+                # Build where clause that looks like this: tran_num = 1 and depth_interp <> -9999
+                delimited_field_id = arcpy.AddFieldDelimiters(self.site.transect_pt_fc, utils.trkCol)
+                where_clause = delimited_field_id + " = %d and " % t
+                where_clause += delimited_field_dep + " <> %d" % utils.nullDep
+                """Get results and find max and min depths
+                    NOTE: Comparisons for max and min look backward because
+                    Depths below MLLW are recorded with negative numbers
+                    So, a maximum depth is actually the lowest number
+                    and minimum depth is the highest number
+                """
+                results = arcpy.da.TableToNumPyArray(self.site.transect_pt_fc, (utils.trkCol,utils.shpDepCol),
+                                                     where_clause)
+                # Might need some checks for null results here.
+                maxdep = results[utils.shpDepCol].min()
+                mindep = results[utils.shpDepCol].max()
+                self.maxtransdeps[t] = maxdep
+                self.mintransdeps[t] = mindep
+
+    def get_vegdeps(self):
+        self.maxvegdeps = {}
+        self.minvegdeps = {}
+        if self.site.transect_pt_fc_exists and self.site.veg_exists:
+            delimited_field_dep = arcpy.AddFieldDelimiters(self.site.transect_pt_fc, utils.shpDepCol)
+            delimited_field_veg = arcpy.AddFieldDelimiters(self.site.transect_pt_fc, self.site.veg_col)
+            delimited_field_video = arcpy.AddFieldDelimiters(self.site.transect_pt_fc, utils.videoCol)
+
+            #results = arcpy.da.TableToNumPyArray(self.site.transect_pt_fc, (utils.trkCol,utils.shpDepCol,utils.videoCol))
+            # print results
+            for t in self.site.transect_list:
+                # Build where clause that looks like this (with proper field delimiters) :
+                # tran_num = 3 and Zm = 1 and video = 1 and depth_interp <> -9999
+                delimited_field_id = arcpy.AddFieldDelimiters(self.site.transect_pt_fc, utils.trkCol)
+                where_clause = delimited_field_id + " = %d and " % t
+                where_clause += delimited_field_veg + " = 1 and "
+                where_clause += delimited_field_video + " = 1 and "
+                where_clause += delimited_field_dep + " <> %d" % utils.nullDep
+                """Get results and find max and min depths
+                    NOTE: Comparisons for max and min look backward because
+                    Depths below MLLW are recorded with negative numbers
+                    So, a maximum depth is actually the lowest number
+                    and minimum depth is the highest number
+                """
+                results = arcpy.da.TableToNumPyArray(self.site.transect_pt_fc, (utils.trkCol,utils.shpDepCol,
+                                                        self.site.veg_col,utils.videoCol),where_clause)
+                # Might need some checks for null results here.
+                maxdep = results[utils.shpDepCol].min()
+                mindep = results[utils.shpDepCol].max()
+                self.maxvegdeps[t] = maxdep
+                self.minvegdeps[t] = mindep
+
+
+
+
+
+class Transect:
+    """ Represents the SVMP data for a transect
+
+    Properties
+    site -- Site object
+    id -- transect id
+    maxdep_flag -- Flag indicating transect can be used for vegetation max depth
+    mindep_flag -- Flag indicating transect can be used for vegetation min depth
+    track_type -- transect type
+    maxdep -- maximum depth of transect
+    mindep -- minimum depth of transect
+    maxdep_veg -- maximum depth of vegetation on transect
+    mindep_veg -- minimum depth of vegetation on transect
+
+
+    """
+    def __init__(self, site, id):
+        """ Pass in a site object to initialize"""
+        self.site = site
+        self.id = id
+
 
 class SiteGroup:
     """ Represents a group of SVMP sites to be processed together with common parameters
@@ -150,17 +337,3 @@ if __name__ == '__main__':
 
     doctest.testmod(verbose=True, report=True)
 
-    mysite = Site("swh1626", "2014", "Native_SG",
-                  r"Z:\Users\allison\projects\dnr_svmp2015\eelgrass_svmp\00-14_monitoring_results\transect_data\svmp_TD_abtest.mdb",
-                  r"Z:\Users\allison\projects\dnr_svmp2015\eelgrass_svmp\00-14_monitoring_results\svmp_site_info.mdb\sample_polygons",
-                  r"Z:\Users\allison\projects\dnr_svmp2015\eelgrass_svmp\2014_test\site_folders")
-
-    print "Transect Feature Class: " + mysite.transect_pt_fc
-    print "Transect Feature Class exists? %s " % (mysite.transect_pt_fc_exists)
-    print "Control File: " + mysite.ctl_file
-    print "Control File exists? %s" % (mysite.ctl_file_exists)
-    print "Veg Column:" + mysite.veg_col
-    print "Veg Column exists? %s" % (mysite.veg_col_exists)
-    print "Veg Exists? %s" % (mysite.veg_exists)
-    print "Sample Polygon ID: %s_%s" % (mysite.id, mysite.sampling_occasion)
-    print "Sample Polygon Exists? %s" % (mysite.sample_poly_exists)
